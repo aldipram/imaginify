573261cf714b7cf80b4205901eb94e472d255ef8
aldipram_ <aldinanda@pop-os.localdomain>
2024-05-07 08:45:07 +0700

    build project upload and delete

--- middleware.ts
+++ middleware.ts
@@ -6,21 +6,3 @@ export default clerkMiddleware()
 export const config = {
   matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-// import {
-//   clerkMiddleware,
-//   createRouteMatcher
-// } from '@clerk/nextjs/server';
-
-// const isProtectedRoute = createRouteMatcher([
-//   '/api/webhooks/clerk',
-//   '/api/webhooks/stripe',
-// ]);
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
